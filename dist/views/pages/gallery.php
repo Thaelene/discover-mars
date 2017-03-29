7ab8e8285bda5cb81f333a8672a6ce52fe758d68
keellyp <kelly.phan@hetic.net>
2017-03-29 15:08:55 +0200

    :art: Add comments, improve gallery's code
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/pages/gallery.php
+++ dist/views/pages/gallery.php
@@ -1,9 +1,16 @@
 <div class="container-dashboard">
+
+<!-- INCLUDE HEADER -->
+
     <?php include 'views/partials/_menu.php' ?>
+
+<!-- GALLERY INFORMATIONS -->
 
     <section class="container">
         <div class="gallery-info">
-            <a href="dashboard" class="back"><i class="fa fa-chevron-left" aria-hidden="true"></i>QUIT GALLERY</a>
+            <a href="dashboard" class="back">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>QUIT GALLERY
+            </a>
             <div class="gallery-title">
                 <div class="info info-title">
                     <h1>Curiosity</h1>
@@ -17,7 +24,18 @@
                     <p>Thursday, March 3th 2017</p>
                 </div>
             </div>
+            <div class="gallery-views">
+                <p for="choose-camera">CHOOSE YOUR VIEW : </p>
+                <select name="choose-camera" id="choose-camera">
+                    <option>Camera 1</option>
+                    <option>Camera 2</option>
+                    <option>Camera 3</option>
+                    <option selected>Toutes les caméras</option>
+                </select>
+            </div>
         </div>
+
+<!-- GALLERY IMAGES -->
 
         <div class="container-image">
             <div class="image-card">
@@ -26,16 +44,6 @@
                 </div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
             </div>
-            <div class="image-card">
-                <div class="image">
-                    <img src="assets/img/logo.png" alt="#">
-                </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
-            </div>
-    
-
         </div>
-
-
     </section>
 </div>
