e3faab8ad0e1aaba67f174e2baae532c8fbc0ee0
keellyp <kelly.phan@hetic.net>
2017-03-29 16:35:32 +0200

    :sparkles: 404 Page
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/pages/404.php
+++ dist/views/pages/404.php
@@ -1,1 +1,13 @@
-<h1>404</h1>
+<div class="error-pages">
+    <div class="error-container">
+        <div class="error-box">
+            <div class="error-content">
+                <p>It seems this page does not exist.</p>
+                <p>Perhaps a martian stole it ? Here is what you are looking for : </p>
+            </div>
+            <div>
+                <a href="dashboard" class="button">CLICK HERE !</a>
+            </div>
+        </div>
+    </div>
+</div>
