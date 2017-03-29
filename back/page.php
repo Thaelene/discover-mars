67fb148837a6d5e5791fec82fc7089e79acab739
thaelene <helene.ta@hetic.net>
2017-03-29 16:14:54 +0200

    :hankey: Test about linking pages
    Signed-off-by: thaelene <helene.ta@hetic.net>

--- back/page.php
+++ back/page.php
@@ -1,3 +1,14 @@
 <?php
+include "handle.php";
 session_start();
 echo $_SESSION['date'];
+?>
+
+<?php foreach ($forecast_photo->photos as $_forecast): ?> <!-- Display each photo infos -->
+    <?php if (($_forecast->camera->name == $camera) || (($_forecast->camera->name == $camera[0]) || ($_forecast->camera->name == $camera[1]) || ($_forecast->camera->name == $camera[2]))) { ?>
+    <div>
+        <br><strong>Image: </strong><img src="<?= $_forecast->img_src ?>">
+        <br><strong>Info: </strong><span>Photographie prise en sol <strong><?= $_forecast->sol ?></strong> (approximativement le <strong><?= Date('d/m/Y', strtotime($date)) ?></strong>) à l'aide de la caméra <strong><?= $_forecast->camera->name ?></strong>.</span>
+    </div>
+    <?php } ?>
+<?php endforeach;  ?>
