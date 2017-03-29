d8a276f8ec92de72d350ab99cd586fd53009ffc2
keellyp <kelly.phan@hetic.net>
2017-03-29 13:25:22 +0200

    :bento: Create gallery
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/pages/gallery.php
+++ dist/views/pages/gallery.php
@@ -2,40 +2,38 @@
     <?php include 'views/partials/_menu.php' ?>
 
     <section class="container">
-        <a href="dashboard"><i class="fa fa-chevron-left" aria-hidden="true"></i>QUIT GALLERY</a>
-
-        <!-- <div class="informations">
-            <div class="info info-title">
-                <h1>Curiosity</h1>
-                <div class="border border-large"></div>
-            </div>
-            <div class="info-bloc">
-                <div class="info info-date">
-                    <h2>Date</h2>
-                    <div class="border border-small"></div>
+        <div class="gallery-info">
+            <a href="dashboard" class="back"><i class="fa fa-chevron-left" aria-hidden="true"></i>QUIT GALLERY</a>
+            <div class="gallery-title">
+                <div class="info info-title">
+                    <h1>Curiosity</h1>
+                    <div class="border border-large"></div>
                 </div>
-                <p>Thursday, March 3th 2017</p>
+                <div class="info-bloc">
+                    <div class="info info-date">
+                        <h2>Date</h2>
+                        <div class="border border-small"></div>
+                    </div>
+                    <p>Thursday, March 3th 2017</p>
+                </div>
             </div>
-        </div> -->
+        </div>
+
         <div class="container-image">
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
+            <div class="image-card">
+                <div class="image">
+                    <img src="assets/img/logo.png" alt="#">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
             </div>
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
+            <div class="image-card">
+                <div class="image">
+                    <img src="assets/img/logo.png" alt="#">
+                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
             </div>
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
-            </div>
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
-            </div>
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
-            </div>
-            <div class="image">
-                <img src="assets/img/logo.png" alt="#">
-            </div>
+    
+
         </div>
 
 
