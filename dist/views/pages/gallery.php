96c73d94d836e3fc185a563f1c0fc052164a3a67
thaelene <helene.ta@hetic.net>
2017-03-29 16:07:45 +0200

    :bento: Call API on gallery page
    Signed-off-by: thaelene <helene.ta@hetic.net>

--- dist/views/pages/gallery.php
+++ dist/views/pages/gallery.php
@@ -24,26 +24,32 @@
                     <p>Thursday, March 3th 2017</p>
                 </div>
             </div>
-            <div class="gallery-views">
-                <p for="choose-camera">CHOOSE YOUR VIEW : </p>
-                <select name="choose-camera" id="choose-camera">
-                    <option>Camera 1</option>
-                    <option>Camera 2</option>
-                    <option>Camera 3</option>
-                    <option selected>Toutes les caméras</option>
+            <form action="#" method="get" class="gallery-views">
+                <input type="date" name="date" value="<?= $date ?>" required name="date"> <!-- Select the date -->
+                <label for="choose-camera">CHOOSE YOUR VIEW :</label>
+                <select name="choose-camera" id="choose-camera" size="1"> <!-- Select the camera and display active camera in the button -->
+                    <option <?php if ($camera == "") { ?>selected<?php } ?> value="">All</option>
+                    <option <?php if ($camera == "FHAZ") { ?>selected<?php } ?> value="FHAZ">FHAZ</option>
+                    <option <?php if ($camera == "RHAZ") { ?>selected<?php } ?> value="RHAZ">RHAZ</option>
+                    <option <?php if ($camera == "NAVCAM") { ?>selected<?php } ?> value="NAVCAM">NAVCAM</option>
                 </select>
-            </div>
+                <input type="submit">
+            </form>
         </div>
 
 <!-- GALLERY IMAGES -->
 
         <div class="container-image">
+            <?php foreach ($forecast_photo->photos as $_forecast): ?> <!-- Display each photo infos -->
+                <?php if (($_forecast->camera->name == $camera) || (($_forecast->camera->name == $camera[0]) || ($_forecast->camera->name == $camera[1]) || ($_forecast->camera->name == $camera[2]))) { ?>
             <div class="image-card">
                 <div class="image">
-                    <img src="assets/img/logo.png" alt="#">
+                    <img src="<?= $_forecast->img_src ?>">
                 </div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,</p>
+                <p>Photographie prise en sol <span><?= $_forecast->sol ?></span>, (approximativement le <span><?= Date('d/m/Y', strtotime($date)) ?></span> à l'aide de la caméra <span><?= $_forecast->camera->name ?></span></p>
             </div>
+            <?php } ?>
+        <?php endforeach; ?>
         </div>
     </section>
 </div>
