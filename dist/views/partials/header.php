14748e8460eea0dc9953c5e133caf6272352909e
keellyp <kelly.phan@hetic.net>
2017-03-29 23:05:16 +0200

    :wheelchair: Add favicon and title
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/partials/header.php
+++ dist/views/partials/header.php
@@ -2,9 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>DISCOVER MARS !</title>
+	<title>DISCOVER MARS</title>
 	<script src="https://use.fontawesome.com/48422937e5.js"></script>
     <link rel="stylesheet" href="<?= URL ?>assets/css/library.min.css">
 	<link rel="stylesheet" href="<?= URL ?>assets/css/style.min.css">
+	<link rel="icon" type="image/png" href="assets/img/favicon.png" />
 </head>
 <body>
