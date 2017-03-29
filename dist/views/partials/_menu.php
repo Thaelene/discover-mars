21989671773e19f7a460815674dcecb73774d99a
keellyp <kelly.phan@hetic.net>
2017-03-29 22:53:51 +0200

    :ok_hand: Edit content of side-bar
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/partials/_menu.php
+++ dist/views/partials/_menu.php
@@ -6,11 +6,11 @@
         <img src="<?= URL ?>assets/img/logo2.png" alt="logo">
         <h3>Brief about the red planet</h3>
         <p>
-            Mars is the 4th planet of the solar system, placed between the Earth and Jupiter, and it is the second smallest planet after Mercury. Its name of Red Planet come from the color of the ground who’s principally composed of iron oxide. It’s a desertic planet but mankind has found ice block near the poles. It possesses a low geological activity and only small events occurred time to time. Its rotation period is almost identical to the Earth, it takes 243 months, or a sol. A Martian year makes 669.5 sol so 686.97 days. Its temperature varies from -3 ° C to -133 ° C.
+            Mars is the 4th planet of the solar system, placed between the Earth and Jupiter, and it is the second smallest planet after Mercury. Its name of Red Planet come from the color of the ground who’s principally composed of iron oxide. It’s a desertic planet but mankind has found ice block near the poles. It possesses a low geological activity and only small events occurred time to time. A Martian year makes 669.5 sol so 686.97 days. Its temperature varies from -3°C to -133°C.
         </p>
         <h3>Who is curiosity ? </h3>
         <p>
-            The remote control arm is articulated to the front of the rover and supports at its end the sampling equipment as well as two scientific instruments. The rear of the housing is occupied by the radioisotope thermoelectric generator overhanging the telecommunications antennas.
+            Curiosity is a car-sized robotic rover exploring Gale Crater on Mars as part of NASA's Mars Science Laboratory mission. It was launched from Cape Canaveral in 2011 and landed a year later. His mission is to investigate on the Martian climate and geology, to know if the site has ever offered environmental conditions favorable for microbial life and seek for answer for a planetary habitability studies in preparation for future human exploration.
         </p>
         <div class="menu-share">
             <a href="https://twitter.com/MarsCuriosity" target="_blank">TWITTER</a><a href="https://www.facebook.com/MarsCuriosity/?fref=ts" target="_blank">FACEBOOK</a>
--- dist/views/partials/header.php
+++ dist/views/partials/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Mon site</title>
+	<title>DISCOVER MARS !</title>
 	<script src="https://use.fontawesome.com/48422937e5.js"></script>
     <link rel="stylesheet" href="<?= URL ?>assets/css/library.min.css">
 	<link rel="stylesheet" href="<?= URL ?>assets/css/style.min.css">
