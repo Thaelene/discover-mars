64dc52994b44ab0597f6c07928478853ef6ce002
thaelene <helene.ta@hetic.net>
2017-03-30 11:56:01 +0200

    :fire: Edit config on api key
    Signed-off-by: thaelene <helene.ta@hetic.net>

--- dist/include/config.php
+++ dist/include/config.php
@@ -1,11 +1,11 @@
 <?php
 
-define('URL', 'http://localhost/GitHub/discover-mars/dist/');
+define('URL', 'http://localhost:8888/discover-mars/dist/');
 define('API_KEY', 'mAf0G7pDruydaGbfRmtxAIVNUCaRWRttre1z2RZB');
 
 // Connexion variables
 define('DB_HOST','localhost');
-define('DB_NAME','discover-mars');
+define('DB_NAME','discover_mars');
 define('DB_USER','root');
 define('DB_PASS','root');
 
