253c48baa39c934517f51e5c39917e87f3fbce3d
keellyp <kelly.phan@hetic.net>
2017-03-30 16:30:00 +0200

    :bento: Add favicon
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/partials/header.php
+++ dist/views/partials/header.php
@@ -6,6 +6,6 @@
 	<script src="https://use.fontawesome.com/48422937e5.js"></script>
     <link rel="stylesheet" href="<?= URL ?>assets/css/library.min.css">
 	<link rel="stylesheet" href="<?= URL ?>assets/css/style.min.css">
-	<link rel="icon" type="image/png" href="assets/img/favicon.png" />
+	<link rel="icon" type="image/png" href="assets/img/favicon2.png" />
 </head>
 <body>
