2e94a29f6588376a201cb7ba427ff2b53f757660
reelwens <sim.lucas27@gmail.com>
2017-03-30 11:16:29 +0200

    :wrench: Modify config

--- dist/include/config.php
+++ dist/include/config.php
@@ -3,22 +3,22 @@
 define('URL', 'http://localhost:8888/discover-mars/dist/');
 
 
-// // Connexion variables
-// define('DB_HOST','localhost');
-// define('DB_NAME','your_dbb'); // Replace 'your_dbb'
-// define('DB_USER','root');
-// define('DB_PASS','root');
-//
-// try
-// {
-//     // Try to connect to database
-//     $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
-//
-//     // Set fetch mode to object
-//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
-// }
-// catch (Exception $e)
-// {
-//     // Failed to connect
-//     die('Could not connect');
-// }
+// Connexion variables
+define('DB_HOST','localhost');
+define('DB_NAME','discover-mars');
+define('DB_USER','root');
+define('DB_PASS','root');
+
+try
+{
+    // Try to connect to database
+    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASS);
+
+    // Set fetch mode to object
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+}
+catch (Exception $e)
+{
+    // Failed to connect
+    die('Could not connect');
+}
