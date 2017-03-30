e7c530d4853d583cbea5b5887de0e3977d2cd35f
reelwens <sim.lucas27@gmail.com>
2017-03-30 23:44:19 +0200

    :fire: Delete API KEY information

--- dist/include/config.php
+++ dist/include/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('URL', 'http://localhost:8888/discover-mars/dist/');
-define('API_KEY', 'mAf0G7pDruydaGbfRmtxAIVNUCaRWRttre1z2RZB');
+define('API_KEY', '');
 
 // Connexion variables
 define('DB_HOST','localhost');
