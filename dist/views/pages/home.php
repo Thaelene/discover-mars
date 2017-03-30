2b15989bfc53134252cccafd48cfe62ebb09086d
keellyp <kelly.phan@hetic.net>
2017-03-30 13:32:36 +0200

    :lipstick: Update Curiosity's style
    Signed-off-by: keellyp <kelly.phan@hetic.net>

--- dist/views/pages/home.php
+++ dist/views/pages/home.php
@@ -17,39 +17,39 @@
             <button type="button"class="button curiosityButton" name="button">LEARN NOW !</button>
         </div>
     </div>
-    <div class="learn-curiosity" id="learn-curiosity">
-        <div class="curiosity-content">
-            <div class="intro">
-                <h2 class="home-title">
-                    My mission is to explore the red planet.
-                </h2>
-                <p>
-                    To do so, the scientist create me, Curiosity. I'm a rover in charge to found water, marks or any other elements who chould provide theory about inhabitant or no living beings.
-                </p>
-                <p>
-                    My mission started the 06 august 2012 and will last 98 weeks. This mission, who’s endowed of a 2.5 billions dollars budget, is exceptional.
-                </p>
+    <div class="learn-curiosity curiosity-container" id="learn-curiosity">
+        <div class="curiosity-first">
+            <div class="curiosity-content">
+                <div class="intro">
+                    <h2 class="home-title">
+                        My mission is to explore the red planet.
+                    </h2>
+                    <p>
+                        To do so, the scientist create me, Curiosity. I'm a rover in charge to found water, marks or any other elements who chould provide theory about inhabitant or no living beings.
+                    </p>
+                    <p>
+                        My mission started the 06 august 2012 and will last 98 weeks. This mission, who’s endowed of a 2.5 billions dollars budget, is exceptional.
+                    </p>
+                </div>
+                <div class="special">
+                    <h2 class="home-title">
+                        Why am I so special ?
+                    </h2>
+                    <p>
+                        I'm going to measure the radiation level on field thanks to my Rad instruments. Despite my size, I can be very precise and I can land in a place that is no flat. My responsible at NASA have chosen to land me in the crater of Gale. Finally, I never sleep. I can work 24 hours a day with my superpowered generator of radioactive plutonium.
+                    </p>
+                </div>
             </div>
-            <div class="special">
-                <h2 class="home-title">
-                    Why am I so special ?
-                </h2>
-                <p>
-                    I'm going to measure the radiation level on field thanks to my Rad instruments. Despite my size, I can be very precise and I can land in a place that is no flat. My responsible at NASA have chosen to land me in the crater of Gale. Finally, I never sleep. I can work 24 hours a day with my superpowered generator of radioactive plutonium.
-                </p>
-            </div>
-            <div class="start">
-                <p>
-                    Now that you are ready, you can start exploring the red planet and know more about the progress of my mission
-                </p>
-                <a href="#"></a>
+            <div class="curiosity-img">
+                <img src="assets/img/curiosity1.png" alt="curiosity-rover">
             </div>
         </div>
-        <div class="curiosity-img">
-            <img src="assets/img/curiosity1.png" alt="curiosity-rover">
+        <div class="curiosity-start">
+            <p>
+                Now that you are ready, you can start exploring the red planet and know more about the progress of my mission
+            </p>
+            <a href="dashboard" class="button">START EXPERIENCE</a>
         </div>
-
-    </div>
     </div>
 </div>
 <script type="text/javascript" src="assets/js/curiosity.js"></script>
